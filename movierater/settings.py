235d1012d01fc889bbc4452cb4d604bdba603b5a
ShazeRx <perkowskikrzysio@gmail.com>
2019-05-18 19:02:24 +0200

    RestAPI Commitv1

--- movierater/settings.py
+++ movierater/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 
-DEBUG = config('DEBUG',default=True,cast=bool)
+DEBUG = config('DEBUG',default=False,cast=bool)
 
 ALLOWED_HOSTS = ['deep-er.herokuapp.com' ,'localhost']
 
